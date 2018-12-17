f05c1065a4f3f90d5519a302d2968462920c1416
maxibor <maxime.borry@gmail.com>
2018-12-17 16:11:19 +0100

    first working prototype

--- sourcepredictlib/forest.py
+++ sourcepredictlib/forest.py
@@ -2,46 +2,40 @@
 
 import pandas as pd
 import numpy as np
-import re
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import normalize as sknormalize
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from . import normalize
 
 
-class sources():
+class sourceforest():
 
-    def __init__(self, infile):
-        self.file = pd.read_csv(infile, index_col=0)
-        self.x = self.file.drop(['label'], axis=1)
-        self.y = self.file.label
-        self.sources = self.file[self.file['label'] != 'sink'].iloc[:, :-1]
-        self.sink = self.file[self.file['label'] == 'sink'].iloc[:, :-1]
-
-    def add_unknown(self, alpha):
-        tmp = self.sink.multiply(alpha)
-        tmp = tmp.apply(np.ceil)
-        tmp = tmp.rename(index=lambda x: re.sub('.*', 'unknown', x))
-        tmp = self.sources.append(tmp)
-        self.train_x = tmp
-        self.y = self.y.append(pd.Series(['Unknown'], index=['Unknown']))
-        self.train_y = self.y.index[self.y != 'sink']
+    def __init__(self, source, sink):
+        self.source = pd.read_csv(source, index_col=0)
+        self.tmp_feat = self.source.drop(
+            ['labels'], axis=0).apply(pd.to_numeric)
+        self.y = self.source.loc['labels', :][1:]
+        self.tmp_sink = pd.read_csv(sink, dtype='int64')
+        self.combined = pd.DataFrame(pd.merge(
+            left=self.tmp_feat, right=self.tmp_sink, how='outer', on='TAXID').drop(['TAXID'], axis=1).fillna(0))
+        return None
 
     def normalize(self):
-        self.train_x = normalize.RLE_normalize(self.train_x)
-        self.train_x = sknormalize(self.train_x)
-        return(self.train_x)
+        print(type(self.combined))
+        self.normalized = normalize.RLE_normalize(self.combined)
+        self.feat = self.normalized.loc[:, self.source.columns[1:]].T
+        self.sink = self.normalized.drop(self.source.columns[1:], axis=1).T
+        return(self.feat, self.sink)
 
     def rndForest(self, seed, threads):
         train_features, test_features, train_labels, test_labels = train_test_split(
-            self.train_x, self.train_y, test_size=0.1)
-        self._forest = RandomForestClassifier(n_jobs=threads, n_estimators=50)
+            self.feat, self.y, test_size=0.2)
+        self._forest = RandomForestClassifier(
+            n_jobs=threads, n_estimators=1000)
+        print("Training classifier")
         self._forest.fit(train_features, train_labels)
-        print(train_features)
-        print(train_labels)
         y_pred = self._forest.predict(test_features)
-        print("Accuracy:", metrics.accuracy_score(test_labels, y_pred))
-        print(test_labels)
-        return(y_pred)
+        print("Training Accuracy:", metrics.accuracy_score(test_labels, y_pred))
+        sink_pred = self._forest.predict_proba(self.sink)
+        print(sink_pred)
