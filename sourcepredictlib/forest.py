1bc6d69adb424df811ac1bd7e6471e32cca30720
Maxime Borry <borry@mpi-sdag1.sdag.ppj.shh.mpg.de>
2019-03-26 16:20:11 +0100

    bray curstis + MDS + KNN for unknown prop

--- sourcepredictlib/forest.py
+++ sourcepredictlib/forest.py
@@ -11,6 +11,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
+from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 from skbio.diversity import beta_diversity
 from skbio.stats.ordination import pcoa as skbio_mds
@@ -57,7 +58,7 @@ class sourceforest():
         for i in range(1, label_avg):
             unk_lab = f"UNKNOWN_{i}"
             unk_labs.append(unk_lab)
-            all_unk.append([int(np.random.normal(x, alpha*x))
+            all_unk.append([int(np.random.normal(x, 0.1))
                             for x in unk_init])
 
         self.unk = pd.DataFrame(data=all_unk).transpose()
@@ -80,14 +81,14 @@ class sourceforest():
                     self.combined, threads)
         self.normalized_unk = pd.merge(left=self.normalized, right=self.unk,
                                        how='outer', left_index=True, right_index=True).fillna(0)
-        # self.feat_unk = self.normalized.drop(self.tmp_sink.columns, axis=1).T
+        self.labels = pd.Series(['known'] * self.normalized.shape[1] +
+                                ['unknown'] * self.unk.shape[1], index=self.normalized_unk.columns, name='labels')
         try:
             self.sink = self.normalized.drop(
                 self.source.columns, axis=1).T
         except KeyError:
             print(f"ERROR: Test sample present in training dataset")
             sys.exit(1)
-        # self.sink = self.sink.loc[:, self.normalized.index]
         self.y_unk = self.y_unk.append(self.unk_labs)
 
     def select_features(self, cv, quantile, seed, threads):
@@ -113,51 +114,83 @@ class sourceforest():
         self.sink_unk = self.pcaed.drop(self.source.columns, axis=0).drop(
             self.unk.columns, axis=0)
 
+    def compute_distance(self, distance_method, rank='species'):
+        # Getting a single Taxonomic rank
+        ncbi = NCBITaxa()
+        only_rank = []
+        for i in list(self.normalized_unk.index):
+            try:
+                if ncbi.get_rank([i])[i] == rank:
+                    only_rank.append(i)
+            except KeyError:
+                continue
+        self.normalized_rank = self.normalized_unk.loc[only_rank, :].T
+        self.skbio_wu = beta_diversity("braycurtis", counts=self.normalized_rank.as_matrix().astype(int), ids=list(
+            self.normalized_rank.index))
+        self.wu = self.skbio_wu.to_data_frame()
+
+    def embed(self, out_csv, seed, n_comp=200):
+        method = 'MDS'
+        cols = [f"PC{i}" for i in range(1, n_comp+1)]
+
+        if method == 'UMAP':
+            embed = umap.UMAP(metric='precomputed',
+                              n_neighbors=30, min_dist=0.03, n_components=n_comp, random_state=seed, n_epochs=500)
+            my_embed = embed.fit(self.wu)
+        elif method == 'TSNE':
+            embed = TSNE(metric='precomputed',
+                         n_components=n_comp, random_state=seed)
+            my_embed = embed.fit(np.matrix(self.wu))
+        elif method == 'MDS':
+            embed = skbio_mds(self.skbio_wu, number_of_dimensions=n_comp)
+            my_embed = pd.DataFrame()
+            for i in range(n_comp):
+                my_embed[f"PC{i+1}"] = list(embed.samples.loc[:, f"PC{i+1}"])
+        else:
+            print(f"Error, {method} embedding method not supported")
+            sys.exit(1)
+
+        if method in (['TSNE', 'UMAP']):
+            self.my_embed = pd.DataFrame(
+                my_embed.embedding_, columns=cols, index=self.wu.index)
+        elif method == 'MDS':
+            self.my_embed = my_embed
+            self.my_embed.set_index(self.wu.index, inplace=True)
+
+        if out_csv:
+            to_write = self.my_embed.copy(deep=True)
+            to_write['labels'] = self.y_unk
+            to_write['name'] = to_write.index
+            to_write.to_csv(out_csv)
+        self.source = self.my_embed.drop(self.tmp_sink.columns, axis=0)
+        self.source = self.source.merge(
+            self.labels, left_index=True, right_index=True)
+        self.sink = self.my_embed.loc[self.tmp_sink.columns, :]
+
     def rndForest(self, seed, threads, outfile, kfold):
         train_features, test_features, train_labels, test_labels = train_test_split(
-            self.feat_unk.drop('label', axis=1), self.feat_unk.loc[:, 'label'], test_size=0.2, random_state=seed)
+            self.source.drop('labels', axis=1), self.source.loc[:, 'labels'], test_size=0.2, random_state=seed)
         train_features, validation_features, train_labels, validation_labels = train_test_split(
-            self.feat_unk.drop('label', axis=1), self.feat_unk.loc[:, 'label'], test_size=0.2, random_state=seed)
-
-        rfc = RandomForestClassifier(random_state=seed, n_jobs=threads)
-
-        param_rf_grid = {
-            'n_estimators': [10, 50, 100],
-            'max_depth': [2, 4, 6]
-        }
-
-        CV_rfc = GridSearchCV(
-            estimator=rfc, param_grid=param_rf_grid, cv=kfold, n_jobs=threads)
+            self.source.drop('labels', axis=1), self.source.loc[:, 'labels'], test_size=0.2, random_state=seed)
 
         print(
-            f"\tPerforming {kfold} fold cross validation on {threads} cores...")
-        CV_rfc.fit(train_features, train_labels)
+            f"\tTraining KNN classifier on {threads} cores...")
 
-        rfc1 = RandomForestClassifier(
-            random_state=seed,
-            max_features='auto',
-            n_estimators=CV_rfc.best_params_['n_estimators'],
-            max_depth=CV_rfc.best_params_['max_depth'],
-            criterion='gini',
-            class_weight="balanced",
-            n_jobs=threads)
-
-        print(
-            f"\tTraining random forest classifier with best parameters on {threads} cores...")
-        rfc1.fit(train_features, train_labels)
-        y_pred = rfc1.predict(test_features)
+        knn1 = KNeighborsClassifier(
+            n_neighbors=len(self.unk_labs), weights='distance', n_jobs=threads)
+        knn1.fit(train_features, train_labels)
+        y_pred = knn1.predict(test_features)
         print("\t-> Testing Accuracy:", round(metrics.accuracy_score(
             test_labels, y_pred), 2))
+        cal_knn = CalibratedClassifierCV(knn1, cv='prefit', method='sigmoid')
+        cal_knn.fit(validation_features, validation_labels)
 
-        cal_rf = CalibratedClassifierCV(rfc1, cv='prefit', method='sigmoid')
-        cal_rf.fit(validation_features, validation_labels)
-
-        self.sink_pred_unk = cal_rf.predict_proba(self.sink_unk)
+        self.sink_pred_unk = cal_knn.predict_proba(self.sink)
         sample = [''.join(list(self.tmp_sink.columns))]
         predictions = utils.class2dict(
-            samples=sample, classes=rfc1.classes_, pred=self.sink_pred_unk)
-        print(
-            f"\t----------------------\n\t- Unknown: {round(predictions[sample[0]]['unknown']*100, 2)}%")
+            samples=sample, classes=cal_knn.classes_, pred=self.sink_pred_unk)
+        utils.print_class(samples=sample,
+                          classes=cal_knn.classes_, pred=self.sink_pred_unk)
         return(predictions)
 
 
