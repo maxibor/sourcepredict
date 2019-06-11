06fee46a5c87313f7230badab43da9649b7aa973
maxibor <maxime.borry@gmail.com>
2019-06-11 17:09:17 +0200

    match more variables with paper

--- sourcepredictlib/ml.py
+++ sourcepredictlib/ml.py
@@ -35,23 +35,23 @@ class sourceunknown():
         Args:
             source(str): training data csv file with OTUs at index, 
                 Samples as columns
-            sink(str): test data csv file with OTUs at index, 
+            sink(pd Dataframe): test data csv file with OTUs at index, 
                 Samples as columns
             labels(str): labels csv file with Samples in first column, 
                 class in 2nd column
 
         """
-        self.source = pd.read_csv(source, index_col=0, dtype='int64')
+        self.ref = pd.read_csv(source, index_col=0, dtype='int64')
         y = pd.read_csv(labels, index_col=0)
         self.y = y['labels']
         self.y_unk = pd.Series(data=['known']*len(list(self.y)), index=y.index)
         self.tmp_sink = sink
         self.combined = pd.DataFrame(pd.merge(
-            left=self.source, right=self.tmp_sink, how='outer', left_index=True, right_index=True).fillna(0))
+            left=self.ref, right=self.tmp_sink, how='outer', left_index=True, right_index=True).fillna(0))
         return None
 
     def __repr__(self):
-        return(f'A sourceforest object of source {self.source} and sink {self.tmp_sink}')
+        return(f'A sourceforest object of source {self.ref} and sink {self.tmp_sink}')
 
     def add_unknown(self, alpha, seed):
         """Add unkown
@@ -68,25 +68,26 @@ class sourceunknown():
         """
 
         np.random.seed = seed
-        label_avg = int(np.average(list(dict(Counter(self.y)).values())))
+        # mean number of samples per class in the reference dataset
+        m_class = int(np.average(list(dict(Counter(self.y)).values())))
 
         tmp_unk = self.tmp_sink.multiply(alpha).apply(np.floor)
         tmp_unk.columns = ["UNKNOWN_0"]
         unk_init = list(tmp_unk.loc[:, "UNKNOWN_0"])
         all_unk = [list(tmp_unk.loc[:, "UNKNOWN_0"])]
-        unk_labs = ["UNKNOWN_0"]
+        ref_u_labs = ["UNKNOWN_0"]
 
-        for i in range(1, label_avg):
+        for i in range(1, m_class):
             unk_lab = f"UNKNOWN_{i}"
-            unk_labs.append(unk_lab)
+            ref_u_labs.append(unk_lab)
             all_unk.append([int(np.random.normal(x, 0.1))
                             for x in unk_init])
 
-        self.unk = pd.DataFrame(data=all_unk).transpose()
-        self.unk.columns = unk_labs
-        self.unk.index = self.tmp_sink.index
-        self.unk_labs = pd.Series(
-            data=['unknown']*len(unk_labs), index=unk_labs)
+        self.ref_u = pd.DataFrame(data=all_unk).transpose()
+        self.ref_u.columns = ref_u_labs
+        self.ref_u.index = self.tmp_sink.index
+        self.ref_u_labs = pd.Series(
+            data=['unknown']*len(ref_u_labs), index=ref_u_labs)
 
     def normalize(self, method, threads):
         """Sample count normalization
@@ -102,24 +103,23 @@ class sourceunknown():
             self.normalized = normalize.RLE_normalize(self.combined)
         elif method == 'SUBSAMPLE':
             self.normalized = normalize.subsample_normalize_pd(self.combined)
-        elif method == 'CLR':
-            self.normalized = normalize.CLR_normalize(self.combined)
         elif method == 'GMPR':
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", RuntimeWarning)
                 self.normalized = normalize.GMPR_normalize(
                     self.combined, threads)
-        self.normalized_unk = pd.merge(left=self.normalized, right=self.unk,
-                                       how='outer', left_index=True, right_index=True).fillna(0)
+
+        self.normalized_ref_u = pd.merge(left=self.normalized, right=self.ref_u,
+                                         how='outer', left_index=True, right_index=True).fillna(0)
         self.labels = pd.Series(['known'] * self.normalized.shape[1] +
-                                ['unknown'] * self.unk.shape[1], index=self.normalized_unk.columns, name='labels')
+                                ['unknown'] * self.ref_u.shape[1], index=self.normalized_ref_u.columns, name='labels')
         try:
             self.sink = self.normalized.drop(
-                self.source.columns, axis=1).T
+                self.ref.columns, axis=1).T
         except KeyError:
             print(f"ERROR: Test sample present in training dataset")
             sys.exit(1)
-        self.y_unk = self.y_unk.append(self.unk_labs)
+        self.y_unk = self.y_unk.append(self.ref_u_labs)
 
     def compute_distance(self, rank='species'):
         """Sample pairwise distance computation
@@ -131,16 +131,16 @@ class sourceunknown():
         # Getting a single Taxonomic rank
         ncbi = NCBITaxa()
         only_rank = []
-        for i in list(self.normalized_unk.index):
+        for i in list(self.normalized_ref_u.index):
             try:
                 if ncbi.get_rank([i])[i] == rank:
                     only_rank.append(i)
             except KeyError:
                 continue
-        self.normalized_rank = self.normalized_unk.loc[only_rank, :].T
+        self.normalized_rank = self.normalized_ref_u.loc[only_rank, :].T
         self.skbio_bc = beta_diversity("braycurtis", counts=self.normalized_rank.as_matrix().astype(int), ids=list(
             self.normalized_rank.index))
-        self.wu = self.skbio_bc.to_data_frame()
+        self.bc = self.skbio_bc.to_data_frame()
 
     def embed(self, out_csv, seed, n_comp=200):
         """
@@ -160,11 +160,10 @@ class sourceunknown():
             my_embed[f"PC{i+1}"] = list(embed.samples.loc[:, f"PC{i+1}"])
 
         self.my_embed = my_embed
-        self.my_embed.set_index(self.wu.index, inplace=True)
+        self.my_embed.set_index(self.bc.index, inplace=True)
 
-        self.source = self.my_embed.drop(self.tmp_sink.columns, axis=0)
-        # self.source['labels'] =
-        self.source = self.source.merge(
+        self.ref_u = self.my_embed.drop(self.tmp_sink.columns, axis=0)
+        self.ref_u = self.ref_u.merge(
             self.labels.to_frame(), left_index=True, right_index=True)
         self.sink = self.my_embed.loc[self.tmp_sink.columns, :]
 
@@ -185,32 +184,32 @@ class sourceunknown():
             seed(int): seed for random number generator
             threads(int): number of threads for parallelization
         Returns:
-            predictions(dict): Probability/proportion of each class
+            dict: Probability/proportion of each class
         """
-        train_features, test_features, train_labels, test_labels = train_test_split(
-            self.source.drop('labels', axis=1), self.source.loc[:, 'labels'], test_size=0.2, random_state=seed)
-        train_features, validation_features, train_labels, validation_labels = train_test_split(
-            train_features, train_labels, test_size=0.2, random_state=seed)
+        train_u_features, test_u_features, train_u_labels, test_u_labels = train_test_split(
+            self.ref_u.drop('labels', axis=1), self.ref_u.loc[:, 'labels'], test_size=0.2, random_state=seed)
+        train_u_features, validation_u_features, train_u_labels, validation_u_labels = train_test_split(
+            train_u_features, train_u_labels, test_size=0.2, random_state=seed)
 
         print(
             f"\tTraining KNN classifier on {threads} cores...")
 
-        knn1 = KNeighborsClassifier(
-            n_neighbors=len(self.unk_labs), weights='distance', n_jobs=threads)
-        knn1.fit(train_features, train_labels)
-        y_pred = knn1.predict(test_features)
+        knn_u = KNeighborsClassifier(
+            n_neighbors=len(self.ref_u_labs), weights='distance', n_jobs=threads)
+        knn_u.fit(train_u_features, train_u_labels)
+        y_pred = knn_u.predict(test_u_features)
         print("\t-> Testing Accuracy:", round(metrics.accuracy_score(
-            test_labels, y_pred), 2))
-        cal_knn = CalibratedClassifierCV(knn1, cv='prefit', method='sigmoid')
-        cal_knn.fit(validation_features, validation_labels)
+            test_u_labels, y_pred), 2))
+        cal_knn_u = CalibratedClassifierCV(knn_u, cv='prefit', method='sigmoid')
+        cal_knn_u.fit(validation_u_features, validation_u_labels)
 
-        self.sink_pred_unk = cal_knn.predict_proba(self.sink)
+        self.sink_pred_unk = cal_knn_u.predict_proba(self.sink)
         sample = [''.join(list(self.tmp_sink.columns))]
-        predictions = utils.class2dict(
-            samples=sample, classes=cal_knn.classes_, pred=self.sink_pred_unk)
+        p_u = utils.class2dict(
+            samples=sample, classes=cal_knn_u.classes_, pred=self.sink_pred_unk)
         utils.print_class(samples=sample,
-                          classes=cal_knn.classes_, pred=self.sink_pred_unk)
-        return(predictions)
+                          classes=cal_knn_u.classes_, pred=self.sink_pred_unk)
+        return(p_u)
 
 
 class sourcemap():
@@ -218,7 +217,7 @@ class sourcemap():
         """Init of sourcemap object
 
         Init of sourcemap object
-        Combines sink and source in one pd Dataframe
+        Combines sink(test) and source(train) in one pd Dataframe
 
         Args:
             source (str): training data csv file with OTUs at index, 
@@ -239,8 +238,6 @@ class sourcemap():
             self.normalized = normalize.RLE_normalize(combined).T
         elif norm_method == 'SUBSAMPLE':
             self.normalized = normalize.subsample_normalize_pd(combined).T
-        elif norm_method == 'CLR':
-            self.normalized = normalize.CLR_normalize(combined).T
         elif norm_method == 'GMPR':
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", RuntimeWarning)
@@ -323,10 +320,10 @@ class sourcemap():
             to_write['name'] = to_write.index
             to_write.to_csv(out_csv)
 
-        self.source = self.my_embed.drop(self.test_samples, axis=0)
-        self.source = self.source.merge(
+        self.ref_t = self.my_embed.drop(self.test_samples, axis=0)
+        self.ref_t = self.ref_t.merge(
             self.labels.to_frame(), left_index=True, right_index=True)
-        self.sink = self.my_embed.drop(self.train_samples, axis=0)
+        self.sink_t = self.my_embed.drop(self.train_samples, axis=0)
 
     def knn_classification(self, kfold, threads, seed):
         """Performs KNN classification
@@ -343,40 +340,41 @@ class sourcemap():
             predictions(dict): Probability/proportion of each class
         """
 
-        train_features, test_features, train_labels, test_labels = train_test_split(
-            self.source.drop('labels', axis=1), self.source.loc[:, 'labels'], test_size=0.2, random_state=seed)
-        train_features, validation_features, train_labels, validation_labels = train_test_split(
-            train_features, train_labels, test_size=0.2, random_state=seed)
+        train_t_features, test_t_features, train_t_labels, test_t_labels = train_test_split(
+            self.ref_t.drop('labels', axis=1), self.ref_t.loc[:, 'labels'], test_size=0.2, random_state=seed)
+        train_t_features, validation_t_features, train_t_labels, validation_t_labels = train_test_split(
+            train_t_features, train_t_labels, test_size=0.2, random_state=seed)
 
-        knn = KNeighborsClassifier(n_jobs=threads)
+        knn_c = KNeighborsClassifier(n_jobs=threads)
 
         param_knn_grid = {
             'n_neighbors': [10, 20, 50]
         }
 
         CV_knn = GridSearchCV(
-            estimator=knn, param_grid=param_knn_grid, cv=kfold, n_jobs=threads)
+            estimator=knn_c, param_grid=param_knn_grid, cv=kfold, n_jobs=threads)
 
         print(
             f"\tPerforming {kfold} fold cross validation on {threads} cores...")
-        CV_knn.fit(train_features, train_labels)
+        CV_knn.fit(train_t_features, train_t_labels)
 
-        knn1 = KNeighborsClassifier(
+        knn2_c = KNeighborsClassifier(
             n_neighbors=CV_knn.best_params_['n_neighbors'], weights='distance', n_jobs=threads)
 
         print(
             f"\tTrained KNN classifier with {CV_knn.best_params_['n_neighbors']} neighbors")
-        knn1.fit(train_features, train_labels)
-        y_pred = knn1.predict(test_features)
+        knn2_c.fit(train_t_features, train_t_labels)
+        y_pred = knn2_c.predict(test_t_features)
         print("\t-> Testing Accuracy:", round(metrics.accuracy_score(
-            test_labels, y_pred), 2))
+            test_t_labels, y_pred), 2))
 
-        cal_knn = CalibratedClassifierCV(knn1, cv='prefit', method='sigmoid')
-        cal_knn.fit(validation_features, validation_labels)
+        cal_knn_c = CalibratedClassifierCV(
+            knn2_c, cv='prefit', method='sigmoid')
+        cal_knn_c.fit(validation_t_features, validation_t_labels)
 
-        self.sink_pred = cal_knn.predict_proba(self.sink)
-        utils.print_class(samples=self.sink.index,
-                          classes=knn1.classes_, pred=self.sink_pred)
-        predictions = utils.class2dict(
-            samples=self.sink.index, classes=knn1.classes_, pred=self.sink_pred)
-        return(predictions)
+        self.sink_pred = cal_knn_c.predict_proba(self.sink_t)
+        utils.print_class(samples=self.sink_t.index,
+                          classes=knn2_c.classes_, pred=self.sink_pred)
+        p_c = utils.class2dict(
+            samples=self.sink_t.index, classes=knn2_c.classes_, pred=self.sink_pred)
+        return(p_c)
